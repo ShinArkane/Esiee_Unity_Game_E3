29c4b8e3cc0fcc58b06bfcca5f4c4a66f653a94e
ShinArkane <47025999+ShinArkane@users.noreply.github.com>
2020-06-17 01:57:56 +0200

    Rangement scene et code

---
+++ Assets/Scripts/personnalCode/Ennemy_yannis.cs
@@ -0,0 +1,72 @@
+﻿using STUDENT_NAME;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ennemy_yannis : MonoBehaviour
+{
+
+    [Tooltip("Vitesse de translation en m.s-1")]
+
+    [SerializeField] float m_TranslationSpeed;
+    Rigidbody m_Rigidbody;
+    [SerializeField] float m_UpRightRotKLerp;
+    static float speed;
+
+    static Vector3 translationVect;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        speed = m_TranslationSpeed;
+        m_Rigidbody = GetComponent<Rigidbody>();
+        translationVect = new Vector3(1, 0, 0) * speed * Time.deltaTime;
+    }
+    void Start()
+    {
+        speed = m_TranslationSpeed;
+
+    }
+
+    public  static Vector3 Speed
+    {
+        get
+        {
+            return translationVect;
+        }
+        set
+        {
+            translationVect = value;
+        }
+
+    }
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Play");
+        if (!GameManager.Instance.IsPlaying) return;
+        //if (!GameManager.IsPlaying) return;
+        //if (!GameManager.IsPlaying) return;
+        transform.Translate(translationVect, Space.Self);
+       
+        m_Rigidbody.MovePosition(transform.position + translationVect);
+        Quaternion qUpright = Quaternion.FromToRotation(transform.up, Vector3.up);
+        Quaternion qNextOrientation = Quaternion.Lerp(transform.rotation, qUpright * transform.rotation, m_UpRightRotKLerp * Time.fixedDeltaTime);
+        m_Rigidbody.MoveRotation(qNextOrientation);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.name.Contains("EnemieWall"))
+
+        {
+            //Debug.Log("collision");
+            translationVect = -translationVect;
+
+        }
+    
+    }
+}
---
+++ Assets/Scripts/personnalCode/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("Vitesse de translation en m.s^-1")]
+    [SerializeField] float m_translationSpeed;
+
+    Rigidbody m_rigidbody;
+
+    private void Awake()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //recuperation de l'imput lier a l'axes horizontal
+        float vInput = Input.GetAxis("Horizontal");
+
+        Vector3 translationVect = vInput * transform.right * m_translationSpeed * Time.fixedDeltaTime;
+        //referentiel world
+
+
+
+
+
+
+
+
+        m_rigidbody.MovePosition(transform.position + translationVect);
+    }
+
+}
